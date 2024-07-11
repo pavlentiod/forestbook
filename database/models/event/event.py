203e89d35fc050c64ec21fa92b96e8bdaa766aa5
pavlentiod <levap.spb@gmail.com>
2024-07-11 16:28:29 +0300

    Set models, db settings and configure db env, migrations and relates between tables

---
+++ database/models/event/event.py
@@ -0,0 +1,23 @@
+import datetime
+from typing import TYPE_CHECKING
+
+from sqlalchemy import String, Integer, Boolean, DateTime, func
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from database.base import Base
+
+if TYPE_CHECKING:
+    from .event_file import Event_file
+    from ..post.post import Post
+
+
+class Event(Base):
+    title: Mapped[str] = mapped_column(String(100), nullable=True, server_default='', default='')
+    count: Mapped[int] = mapped_column(Integer, nullable=False)
+    split_link: Mapped[str] = mapped_column(String(200), nullable=True, unique=False)
+    date: Mapped[datetime.datetime] = mapped_column(
+        DateTime(timezone=True), server_default=func.now())
+    status: Mapped[bool] = mapped_column(Boolean, nullable=False)
+
+    event_files: Mapped["Event_file"] = relationship(back_populates="event", cascade="all, delete")
+    posts: Mapped[list["Post"]] = relationship(back_populates="event")
