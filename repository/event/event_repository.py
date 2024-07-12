38bd80843221255a35ab781fd98a518b87522301
pavlentiod <levap.spb@gmail.com>
2024-07-12 11:16:57 +0300

    Update repository v1

--- repository/event/event_repository.py
+++ repository/event/event_repository.py
@@ -1,15 +1,23 @@
-from sqlalchemy.orm import Session
+import asyncio
+import datetime
+import uuid
+
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from database import db_helper
 from database.models.event.event import Event
+from schemas.event.event_file_schema import EventFileOutput
 from schemas.event.event_schema import EventInput, EventOutput
-from typing import List, Optional
+from typing import List, Optional, Type
 from pydantic import UUID4
 
 
 class EventRepository:
-    def __init__(self, session: Session):
+    def __init__(self, session: AsyncSession):
         self.session = session
 
-    def create(self, data: EventInput) -> EventOutput:
+    async def create(self, data: EventInput) -> EventOutput:
         event = Event(
             title=data.title,
             count=data.count,
@@ -18,8 +26,8 @@ class EventRepository:
             status=data.status
         )
         self.session.add(event)
-        self.session.commit()
-        self.session.refresh(event)
+        await self.session.commit()
+        await self.session.refresh(event)
         return EventOutput(
             id=event.id,
             title=event.title,
@@ -27,36 +35,58 @@ class EventRepository:
             split_link=event.split_link,
             date=event.date,
             status=event.status,
-            event_files=[],
-            posts=[]
         )
 
-    def get_all(self) -> List[Optional[EventOutput]]:
-        events = self.session.query(Event).all()
+    async def get_all(self) -> List[Optional[EventOutput]]:
+        stmt = select(Event).order_by(Event.id)
+        result = await self.session.execute(stmt)
+        events = result.scalars().all()
         return [EventOutput(**event.__dict__) for event in events]
 
-    def get_event(self, event_id: UUID4) -> EventOutput:
-        event = self.session.query(Event).filter_by(id=event_id).first()
+    async def get_event(self, _id: UUID4) -> EventOutput:
+        event = await self.session.get(Event, _id)
         return EventOutput(**event.__dict__)
 
-    def get_by_id(self, event_id: UUID4) -> Optional[Event]:
-        return self.session.query(Event).filter_by(id=event_id).first()
+    async def get_by_id(self, _id: UUID4) -> Optional[Event]:
+        return await self.session.get(Event, _id)
 
-    def event_exists_by_id(self, event_id: UUID4) -> bool:
-        event = self.session.query(Event).filter_by(id=event_id).first()
+    async def event_exists_by_id(self, _id: UUID4) -> bool:
+        event = await self.session.get(Event, _id)
         return event is not None
 
-    def update(self, event: Event, data: EventInput) -> EventOutput:
-        event.title = data.title
-        event.count = data.count
-        event.split_link = data.split_link
-        event.date = data.date
-        event.status = data.status
-        self.session.commit()
-        self.session.refresh(event)
+    async def update(self, event: Type[Event], data: EventInput) -> EventOutput:
+        for key, value in data.model_dump(exclude_none=True).items():
+            setattr(event, key, value)
+        await self.session.commit()
+        await self.session.refresh(event)
         return EventOutput(**event.__dict__)
 
-    def delete(self, event: Event) -> bool:
-        self.session.delete(event)
-        self.session.commit()
+    async def delete(self, event: Type[Event]) -> bool:
+        await self.session.delete(event)
+        await self.session.commit()
         return True
+
+
+# async def main():
+#     async with db_helper.session_factory() as session:
+#         ev = EventRepository(session)
+#         inp = EventInput(
+#             title="Sample Event",
+#             count=11,
+#             status=True,
+#             split_link="https://example.com",
+#             date="2024-07-12T12:00:00"
+#         )
+#         inp2 = EventInput(
+#             title="Updated Event",
+#             count=13,
+#             status=False,
+#             split_link="https://upd-example.com",
+#             date=datetime.datetime.now(tz=datetime.timezone.utc)
+#         )
+#         event = await ev.get_by_id("1ae075c1-aff0-4c38-acf5-0b75e25c4bc7")
+#         await ev.update(event,inp2)
+#
+#
+# if __name__ == "__main__":
+#     asyncio.run(main())
