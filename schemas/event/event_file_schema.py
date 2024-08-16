3867160bbccf0f205cb99f898709a5414b5fed84
pavlentiod <levap.spb@gmail.com>
2024-08-16 18:06:32 +0300

    Filenames fill automatically in event_files model

--- schemas/event/event_file_schema.py
+++ schemas/event/event_file_schema.py
@@ -1,13 +1,15 @@
 from typing import TYPE_CHECKING
 
 from pydantic import BaseModel, Field
-from uuid import UUID
+from uuid import UUID, uuid4
+
+from config import settings
 
 
 class EventFileInput(BaseModel):
-    splits_path: str = Field(max_length=200)
-    routes_path: str = Field(max_length=200)
-    results_path: str = Field(max_length=200)
+    splits_path: str = Field(max_length=200, default=f'{settings.aws.AWS_SPLITS_PATH}{str(uuid4())}.json')
+    routes_path: str = Field(max_length=200, default=f'{settings.aws.AWS_ROUTES_PATH}{str(uuid4())}.json')
+    results_path: str = Field(max_length=200, default=f'{settings.aws.AWS_RESULTS_PATH}{str(uuid4())}.json')
     event_id: UUID
 
 
