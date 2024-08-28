01dd219f68b362da0cc88a28ee64452672d6e054
pavlentiod <levap.spb@gmail.com>
2024-08-28 11:22:10 +0300

    Remove results calculating from parse process

--- src/services/parser/src/parser.py
+++ src/services/parser/src/parser.py
@@ -1,5 +1,4 @@
 import requests
-import pandas as pd
 from bs4 import BeautifulSoup
 from pandas import DataFrame
 
@@ -8,16 +7,6 @@ from src.services.parser.src.SPORT_ORG_parse import SO_parsing
 from src.services.parser.src.WinOrient_parse import SI_parsing
 from src.services.parser.src.utils import bs
 
-
-def calculate_results(splits_df: DataFrame) -> dict:
-    splits_df['group'] = [i.split('^')[-1] for i in splits_df.index]
-    df = splits_df.loc[:, ('group', 'RES')]
-    df['RES'] = df['RES'].apply(lambda x: x.total_seconds() if x != pd.NaT else 'DSQ')
-    results = {}
-    for i in df.groupby('group')['RES']:
-        vals = i[1].sort_values(ascending=True)
-        results.setdefault(i[0], dict(zip(vals.index, vals.tolist())))
-    return results
 
 def parse_event(page: BeautifulSoup) -> (DataFrame, dict):
     title = str(page.find('title'))
