5d835f185fa01689685f452fe3809cb434a52418
pavlentiod <levap.spb@gmail.com>
2024-08-28 11:15:14 +0300

    Edit base class with "extend_existing" for add new changes to exiting

--- src/database/base.py
+++ src/database/base.py
@@ -10,3 +10,4 @@ class Base(DeclarativeBase):
         return f"{cls.__name__.lower()}s"
 
     id: Mapped[str] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    __table_args__ = {'extend_existing': True}
