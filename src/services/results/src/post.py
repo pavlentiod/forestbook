3653b4b2af3bdd34903d3a1fdb2500a010ebe65f
pavlentiod <levap.spb@gmail.com>
2024-08-28 11:25:25 +0300

    Remove post calculating and split formatting files

--- src/services/results/src/post.py
+++
@@ -1,9 +0,0 @@
-from src.schemas.post.post_schema import PostInput
-from src.schemas.results.results_schema import Results
-from src.schemas.split.split_schema import SplitInput
-
-
-def calc_post(split: SplitInput, results: Results) -> PostInput:
-    pass
-
-
