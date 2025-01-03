a5b8be7306fab99ae2076c3cb75073ab576afe2f
pavel <levap.spb@gmail.com>
2025-01-03 17:52:57 +0300

    General.tic_00 refactor and remove unused

--- src/routers/__init__.py
+++ src/routers/__init__.py
@@ -6,7 +6,3 @@ router = APIRouter()
 
 router.include_router(user_router, prefix="/users", tags=['Users'])
 
-# TODO: Post.tic_01  Add filters to get_all request
-# TODO: Team.tic_01  Add filters to get_all request
-# TODO: Team_member.tic_01  Add filters to get_all request
-# TODO: User.tic_01  Add filters to get_all request
