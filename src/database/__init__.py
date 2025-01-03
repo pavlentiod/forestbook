8d39ae498e6d130c115c95c47534db3cf4058c9c
pavel <levap.spb@gmail.com>
2025-01-03 17:46:24 +0300

    General.tic_01 fix imports

---
+++ src/database/__init__.py
@@ -0,0 +1,6 @@
+from src.database.base import Base
+from src.database.db_helper import db_helper
+from src.database.models import Post, User
+
+
+__all__=[db_helper, Base]
--- src/database/models/__init__.py
+++ src/database/models/__init__.py
@@ -1,7 +1,4 @@
 
-# TODO: Team.tic_00  Create model and relates
-# TODO: Post.tic_00  Create model and relates
-
 
 from .post.post import Post
 from .user.user import User
